9f722a00d0c9571317f045433b58c0f52c07b109
Emilien Macchi <emacchi@redhat.com>
2024-11-06 14:22:21 -0500

    openshift: add v1alpha1 to scheme

--- openshift/pkg/scheme/scheme.go
+++ openshift/pkg/scheme/scheme.go
@@ -8,7 +8,8 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 
-	infrav1 "sigs.k8s.io/cluster-api-provider-openstack/api/v1beta1"
+	infrav1alpha1 "sigs.k8s.io/cluster-api-provider-openstack/api/v1alpha1"
+	infrav1beta1 "sigs.k8s.io/cluster-api-provider-openstack/api/v1beta1"
 )
 
 func DefaultScheme() *runtime.Scheme {
@@ -16,7 +17,8 @@ func DefaultScheme() *runtime.Scheme {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(openshiftconfig.AddToScheme(scheme))
 	utilruntime.Must(machinev1beta1.AddToScheme(scheme))
-	utilruntime.Must(infrav1.AddToScheme(scheme))
+	utilruntime.Must(infrav1alpha1.AddToScheme(scheme))
+	utilruntime.Must(infrav1beta1.AddToScheme(scheme))
 	utilruntime.Must(clusterv1.AddToScheme(scheme))
 
 	return scheme
