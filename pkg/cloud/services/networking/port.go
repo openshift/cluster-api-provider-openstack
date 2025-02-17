9f131c6bf83e71022ac2596ef14a8475de023ff4
Emilien Macchi <emacchi@redhat.com>
2025-02-17 10:15:51 -0500

    Revert "CARRY: Revert to Go v1.21"
    This reverts commit dd74d6489b30e4c5407b82ddf2f6e807ae9aac30.

--- pkg/cloud/services/networking/port.go
+++ pkg/cloud/services/networking/port.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"slices"
 	"strings"
 	"time"
 
@@ -33,7 +34,6 @@ import (
 
 	infrav1alpha1 "sigs.k8s.io/cluster-api-provider-openstack/api/v1alpha1"
 	infrav1 "sigs.k8s.io/cluster-api-provider-openstack/api/v1beta1"
-	"sigs.k8s.io/cluster-api-provider-openstack/internal/futures"
 	"sigs.k8s.io/cluster-api-provider-openstack/pkg/record"
 	"sigs.k8s.io/cluster-api-provider-openstack/pkg/scope"
 	capoerrors "sigs.k8s.io/cluster-api-provider-openstack/pkg/utils/errors"
@@ -469,7 +469,7 @@ func (s *Service) normalizePorts(ports []infrav1.PortOpts, clusterResourceName, 
 		}
 
 		// Tags are inherited base tags plus any port-specific tags
-		normalizedPort.Tags = futures.SlicesConcat(baseTags, port.Tags)
+		normalizedPort.Tags = slices.Concat(baseTags, port.Tags)
 
 		// No Trunk field specified for the port, inherit the machine default
 		if port.Trunk == nil {
